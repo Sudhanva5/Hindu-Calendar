4b5d4f7fa78937b5fd3fa3852a7088259035e3ac
Sudhanva Acharya <sudhanva.udupi55@gmail.com>
2025-04-24 14:58:44 +0530

    Optimisation Fixes

--- Om Calendar/Services/LocationService.swift
+++ Om Calendar/Services/LocationService.swift
@@ -1,4 +1,5 @@
 import CoreLocation
+import UIKit
 
 class LocationService: NSObject, ObservableObject {
     static let shared = LocationService()
@@ -12,11 +13,13 @@ class LocationService: NSObject, ObservableObject {
         manager = CLLocationManager()
         super.init()
         
-        // Configure location manager
+        // Configure location manager with battery-efficient settings
         manager.delegate = self
-        manager.desiredAccuracy = kCLLocationAccuracyBest
-        manager.allowsBackgroundLocationUpdates = true
-        manager.showsBackgroundLocationIndicator = true
+        manager.desiredAccuracy = kCLLocationAccuracyKilometer  // Reduced accuracy (1km is enough for panchanga)
+        manager.distanceFilter = 10000  // Only update if moved more than 10km
+        manager.allowsBackgroundLocationUpdates = false  // Disable background updates
+        manager.showsBackgroundLocationIndicator = false
+        manager.pausesLocationUpdatesAutomatically = true
         
         // Get initial status
         authorizationStatus = manager.authorizationStatus
@@ -26,8 +29,40 @@ class LocationService: NSObject, ObservableObject {
             manager.requestWhenInUseAuthorization()
         } else if manager.authorizationStatus == .authorizedWhenInUse || 
                   manager.authorizationStatus == .authorizedAlways {
-            manager.startUpdatingLocation()
+            manager.requestLocation()
         }
+        
+        // Setup notifications for app state changes
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(appMovedToBackground),
+            name: UIApplication.didEnterBackgroundNotification,
+            object: nil
+        )
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(appMovedToForeground),
+            name: UIApplication.willEnterForegroundNotification,
+            object: nil
+        )
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc private func appMovedToBackground() {
+        manager.stopUpdatingLocation()
+    }
+    
+    @objc private func appMovedToForeground() {
+        manager.requestLocation()
+    }
+    
+    // Function to manually request a location update
+    func requestLocationUpdate() {
+        manager.requestLocation()  // Single update
     }
 }
 
@@ -37,7 +72,7 @@ extension LocationService: CLLocationManagerDelegate {
         
         switch manager.authorizationStatus {
         case .authorizedWhenInUse, .authorizedAlways:
-            manager.startUpdatingLocation()
+            manager.requestLocation()
         default:
             break
         }
@@ -46,5 +81,9 @@ extension LocationService: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
         self.location = location
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location update failed: \(error.localizedDescription)")
     }
 } 
